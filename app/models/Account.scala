426428e467d3d96368f1844066e696ae07a4eac6
Yuriy Zavada <yuriy.zavada@benify.com>
2015-10-16 11:34:46 +0200

    user handling

--- app/models/Account.scala
+++ app/models/Account.scala
@@ -1,27 +1,41 @@
 package models
 
+import services.FileService
+import play.api.libs.json.{JsArray, Json, JsValue, Reads}
+
 case class Account(id: Int, email: String, password: String, name: String, role: Role)
 
 object Account {
+  
+  
+  
+  
+  val fileService = new FileService
 
   def authenticate(email: String, password: String): Option[Account] = {
     findByEmail(email)
   }
 
   def findByEmail(email: String): Option[Account] = {
-    Option.apply(new Account(1, "test@gmail.com", "test", "test", Role.NormalUser))
+    findAll.find(account => account.email == email)
   }
 
   def findById(id: Int): Option[Account] = {
-    Option.apply(new Account(1, "test@gmail.com", "test", "test", Role.NormalUser))
+    findAll.find(account => account.id == id)    
   }
-
-  def findAll(): Seq[Account] = { 
-    null
-  }
-
+  
+ def findAll(): Seq[Account] = {    
+   val jsUsers : Seq[JsValue] = Json.parse(fileService.readFromFile("users.txt")).as[JsArray].value
+   for(jsValue <- jsUsers) yield createAccount(jsValue)
+}
+  
   def create(account: Account) {
    
+  }
+  
+  def createAccount(jsValue: JsValue): Account ={
+    Account((jsValue \ "id").as[String].toInt, (jsValue \ "email").as[String], (jsValue \ "password").as[String], 
+            (jsValue \ "name").as[String], Role.valueOf((jsValue \ "role").as[String]))
   }
   
 }
