fc04ea049be81ab91c4ff7186c3868df423c921e
Yurko <yuriy.zavada@benify.com>
2015-10-27 20:24:19 +0100

    test

--- app/controllers/AuthConfigImpl.scala
+++ app/controllers/AuthConfigImpl.scala
@@ -2,13 +2,12 @@ package controllers
 
 import play.api.mvc.RequestHeader
 import play.api.mvc.Results._
+import jp.t2v.lab.play2.auth.{CookieTokenAccessor, TokenAccessor}
 
 import scala.concurrent.{Future, ExecutionContext}
 
 trait AuthConfigImpl extends BaseAuthConfig {
   
-//  override lazy val cookieSecureOption: Boolean = false
-
   def loginSucceeded(request: RequestHeader)(implicit ctx: ExecutionContext) = Future.successful(Redirect(routes.Messages.main))
 
   def logoutSucceeded(request: RequestHeader)(implicit ctx: ExecutionContext) = Future.successful(Redirect(routes.Application.index))
@@ -16,6 +15,15 @@ trait AuthConfigImpl extends BaseAuthConfig {
   def authenticationFailed(request: RequestHeader)(implicit ctx: ExecutionContext) = {
     Future.successful(Redirect(routes.Sessions.login))
   }
+  
+  override lazy val tokenAccessor: TokenAccessor = new CookieTokenAccessor(
+    cookieName = "PLAY2AUTH_SESS_ID",
+    cookieSecureOption = false,
+    cookieHttpOnlyOption = true,
+    cookieDomainOption = None,
+    cookiePathOption = "/",
+    cookieMaxAge = None
+  )
 
 
 }
