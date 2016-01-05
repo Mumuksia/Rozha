3c5671b4e03d0be3ef899781c5a35c619278a62f
Yuriy Zavada <yuriy.zavada@benify.com>
2016-01-05 14:44:05 +0100

    expand users notes

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -15,19 +15,22 @@ import services.UserDTO
 class UsersController  extends Controller{
   
   val jsonService = new JsonService
-
+  var uName: String = "Muksia"
+  
 def loadUsers = Action {
   request => 
-  Ok(jsonService.transformUsersDTOAndPenaltiesToJsObject(UserDTO.getAllUsersDTO(Notes.findAll, User.findAll), Notes.findAllForUser("Muksia")))
+  Ok(jsonService.transformUsersDTOAndPenaltiesToJsObject(UserDTO.getAllUsersDTO(Notes.findAll, User.findAll), Notes.findAllForUser(uName)))
 }  
 
 def addUser(clanId: String, name: String, status: String) = Action{
     User.create(clanId, name, status, "some")
+    uName = name
     Ok(jsonService.transformUsersDTOAndPenaltiesToJsObject(UserDTO.getAllUsersDTO(Notes.findAll, User.findAll), Notes.findAllForUser(name)))
 }
 
 def loadUsersWithUser(userName: String) = Action{
       request => 
+      uName = userName
   Ok(jsonService.transformUsersDTOAndPenaltiesToJsObject(UserDTO.getAllUsersDTO(Notes.findAll, User.findAll), Notes.findAllForUser(userName)))
 }
   
